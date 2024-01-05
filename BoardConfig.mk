a72ee36b373624b7d42bda6b3843e42f48a3de80
Alexander Winkowski <dereference23@outlook.com>
2024-01-05 12:09:54 +0000

    miuicamera: Create MiuiCamera SELinux domain
    Change-Id: I43b64ff3edfac0c2183d4ecedc42a04534924797

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,5 +9,8 @@ MIUICAMERA_PATH := device/xiaomi/duchamp-miuicamera
 # Properties
 TARGET_SYSTEM_EXT_PROP += $(MIUICAMERA_PATH)/system_ext.prop
 
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
+
 # Inherit from the proprietary version
 include vendor/xiaomi/duchamp-miuicamera/BoardConfigVendor.mk
