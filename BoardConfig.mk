d39e94860f98e43f3f81e569b2e9697ec0015f35
rio004 <riyon336@gmail.com>
2025-02-12 17:53:09 +0000

    miuicamera: Label `ro.product.mod_device`
    Change-Id: I48e662dfc87d2be6b1ae677ba6d82e0bef55a062
    Signed-off-by: rio004 <riyon336@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,7 @@ TARGET_SYSTEM_EXT_PROP += $(MIUICAMERA_PATH)/system_ext.prop
 
 # SELinux
 BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Inherit from the proprietary version
 include vendor/xiaomi/duchamp-miuicamera/BoardConfigVendor.mk
